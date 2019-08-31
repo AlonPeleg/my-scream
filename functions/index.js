81c0fb5df6240b4a4de1fcce8708775b7ea27347
Alon Peleg <peleg619@gmail.com>
2019-08-31 05:02:33 +0300

    added express

--- functions/index.js
+++ functions/index.js
@@ -3,45 +3,43 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const express = require("express");
+const app = express();
 
-exports.getScreams = functions
-  .region("europe-west1")
-  .https.onRequest((req, res) => {
-    admin
+app.get("/screams", async (req, res) => {
+  try {
+    const data = await admin
       .firestore()
       .collection("screams")
-      .get()
-      .then(data => {
-        let screams = [];
-        data.forEach(doc => {
-          screams.push(doc.data());
-        });
-        return res.json(screams);
-      })
-      .catch(err => console.error(err));
-  });
+      .get();
+    let screams = [];
+    data.forEach(doc => {
+      screams.push({
+        screamId: doc.id,
+        ...doc.data()
+      });
+    });
+    return res.json(screams);
+  } catch (err) {
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
-exports.createScream = functions
-  .region("europe-west1")
-  .https.onRequest((req, res) => {
-    if (req.method !== "POST") {
-      return res.status(400).json({ error: "Method not allowed" });
-    }
-    const newScream = {
-      body: req.body.body,
-      userHandle: req.body.userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date())
-    };
-
-    admin
+app.post("/scream", async (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  try {
+    const doc = await admin
       .firestore()
       .collection("screams")
-      .add(newScream)
-      .then(doc => {
-        res.json({ message: `document ${doc.id} created successfully` });
-      })
-      .catch(err => res.status(500).json({ error: "Server Error" }));
-  });
+      .add(newScream);
+    res.json({ message: `document ${doc.id} created successfully` });
+  } catch (err) {
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
